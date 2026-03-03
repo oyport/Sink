9837c24ed4d36f13a710268ac99b3b21a4c64be5
Ofine <61049989+oyport@users.noreply.github.com>
2026-03-03 14:14:59 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   title: 'ToShort - To.cd',
   github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
+  coffee: 'https://to.td/coffee',
+  twitter: 'https://to.td/x',
   telegram: 'https://sink.cool/telegram',
   description: 'To.cd is a concise and efficient URL shortening service platform that provides fast, secure, and stable link shortening services, supporting features like custom short links, data statistics, and QR code generation, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
